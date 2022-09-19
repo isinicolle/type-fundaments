c8a791abde48ff1358648ca0feef0cd94ad65953
Isis Florentino <isis.zapata.hn@gmail.com>
2022-09-19 08:31:01 -0600

    Update demo.js
    npx tsc src/demo.js --target es6

--- fundamentos/src/demo.js
+++ fundamentos/src/demo.js
@@ -1,39 +1,38 @@
 //@ts-check
-
 (async ()=> {
-    const myCart = [];
-    const products = [];
-    const limit = 2;
+  const myCart = [];
+  const products = [];
+  const limit = 2;
 
-    async function getProducts() {
-      const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
-        method: 'GET'
-      });
-      const data = await rta.json();
-      products.concat(data);
+  async function getProducts() {
+    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
+      method: 'GET'
+    });
+    const data = await rta.json();
+    products.concat(data);
+  }
+  function getTotal() {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+      total += products[i].prize;
     }
-    function getTotal() {
-      const total = 0;
-      for (let i = 0; i < products.length; i++) {
-        total += products[i].prize;
-      }
-      return total;
+    return total;
+  }
+  function addProduct(index) {
+    if (getTotal() <= limit) {
+      myCart.push(products[index]);
     }
-    function addProduct(index) {
-      if (getTotal <= limit) {
-        myCart.push(products[index]);
-      }
-    }
+  }
 
-    await getProducts();
-    addProducto(1);
-    addProducto(2);
-    const total = getTotal();
-    console.log(total);
-    const person = {
-      name: 'Nicolas',
-      lastName: 'Molina'
-    }
-    const rta = person +  limit;
-    console.log(rta);
-  });
+  await getProducts();
+  addProduct(1);
+  addProduct(2);
+  const total = getTotal();
+  console.log(total);
+  const person = {
+    name: 'Nicolas',
+    lastName: 'Molina'
+  }
+  //const rta = person +  limit;
+  //console.log(rta);
+});
